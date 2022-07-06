7dc57a5102c76630d5c71086e706718ea139f6b0
beza <bzendale6@gmail.com>
2022-07-06 10:10:06 -0500

    paremeter calculater

---
+++ Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cassignment3
+{
+    static void Nain(String[] Args) {
+     class Ishape
+    {
+        public double Y { get; }
+
+         abstract calculate_area(double x, double I);
+        
+            
+        
+
+        abstract calculate_parameter();
+
+
+    }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cassignment3
+{
+    class Program
+    {
+            
+             class Circle
+        {
+            
+            public Calculate_area(double x,double I)
+            {
+                double r, per_cir;
+                double PI = 3.14;
+                Console.WriteLine("Input the radius of the circle : ");
+                r = Convert.ToDouble(Console.ReadLine());
+                per_cir = 2 * PI * r;
+                Console.WriteLine("Perimeter of Circle : {0}", per_cir);
+                Console.Read();
+            }
+
+        }
+        class rectangle
+        {
+            public int  width;
+            public int height;
+
+
+        }
+
+        }
+    
+
+
+}
